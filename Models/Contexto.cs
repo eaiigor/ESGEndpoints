18e871cf6f2f3fb9a6243e021590b4a41c2e56e9
Igor Rocha Lima <contatoigorrochalima@hotmail.com>
2024-08-10 11:20:20 -0300

    feat(model-tarefas): Criando model de tarefas

---
+++ Models/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ESGENDPOINTS.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Tarefa> Tarefa { get; set; }
+    }
+}
