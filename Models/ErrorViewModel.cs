c8719de200f5ebe9e68a86482a5893961e8c4cd0
Igor Rocha Lima <contatoigorrochalima@hotmail.com>
2024-08-10 10:08:24 -0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ESGENDPOINTS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
