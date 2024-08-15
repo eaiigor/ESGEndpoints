1ca32e47c2961021259ddbce556a7c81f461b342
Igor Rocha Lima <contatoigorrochalima@hotmail.com>
2024-08-15 01:15:16 -0300

    feat(Controllers): Criação dos endpoints de tarefas

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,15 @@
-﻿using ESGENDPOINTS.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace ESGENDPOINTS.Controllers
 {
-    public class HomeController : Controller
+    [ApiController]
+    [Route("/")]
+    public class HomeController : ControllerBase
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return Ok(new { message = "running " });
         }
     }
 }
---
+++ Controllers/TarefasController.cs
@@ -0,0 +1,89 @@
+﻿using ESGENDPOINTS.Application.Commands;
+using ESGENDPOINTS.Application.Queries;
+using ESGENDPOINTS.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ESGENDPOINTS.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TarefasController : ControllerBase
+    {
+
+        public TarefasController() {}
+
+        // GET: api/Tarefas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Tarefa>>> GetTarefas(
+            [FromServices] ITarefasQueries queries) => Ok(await queries.GetAllAsync());
+
+        // GET: api/Tarefas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Tarefa>> GetTarefa(int id, [FromServices] ITarefasQueries queries)
+        {
+            var tarefa = await queries.GetByIdAsync(id);
+
+            if (tarefa == null)
+            {
+                return NotFound();
+            }
+
+            return tarefa;
+        }
+
+        // POST: api/Tarefas
+        [HttpPost]
+        public async Task<ActionResult<Tarefa>> PostTarefa([FromBody] CriarTarefaCommand command, 
+            [FromServices] IMediator mediator)
+        {
+            if (command == null)
+            {
+                return BadRequest("Tarefa não pode ser nula.");
+            }
+
+            var tarefa = await mediator.Send(command);
+            return CreatedAtAction(nameof(GetTarefa), new { id = tarefa.Id }, tarefa);
+        }
+
+        // PUT: api/Tarefas/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTarefa(int id, 
+            [FromBody] EditarTarefaCommand command, 
+            [FromServices] IMediator mediator,
+            [FromServices] ITarefasQueries queries)
+        {
+            if (id != command.Id)
+            {
+                return BadRequest();
+            }            
+
+            if(!(await queries.ExistsAsync(id)))
+            {
+                return NotFound();
+            }
+
+            return Ok(await mediator.Send(command));
+        }
+
+        // DELETE: api/Tarefas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTarefa(int id,
+            [FromServices] IMediator mediator,
+            [FromServices] ITarefasQueries queries)
+        {
+            if (!(await queries.ExistsAsync(id)))
+            {
+                return NotFound();
+            }
+
+            var cmd = new DeletarTarefaCommand()
+            {
+                Id = id,
+            };
+
+            await mediator.Send(cmd);
+            return NoContent();
+        }
+    }
+}
