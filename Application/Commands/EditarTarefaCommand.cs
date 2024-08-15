03e08c42be5aa6789b1a727a59e19375d15c750d
Igor Rocha Lima <contatoigorrochalima@hotmail.com>
2024-08-15 01:15:04 -0300

    feat(EditarTarefa): Command/Handler

---
+++ Application/Commands/EditarTarefaCommand.cs
@@ -0,0 +1,15 @@
+﻿using ESGENDPOINTS.Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ESGENDPOINTS.Application.Commands
+{
+    public class EditarTarefaCommand: IRequest<Tarefa>
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string? Descricao { get; set; }
+        public DateTime DataVencimento { get; set; }
+        public int Status { get; set; }
+    }
+}
---
+++ Application/Handlers/EditarTarefaHandler.cs
@@ -0,0 +1,32 @@
+﻿using ESGENDPOINTS.Application.Commands;
+using ESGENDPOINTS.Domain.Entities;
+using ESGENDPOINTS.Infrastructure.Repositories;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace ESGENDPOINTS.Application.Handlers
+{
+    public class EditarTarefaHandler : IRequestHandler<EditarTarefaCommand, Tarefa>
+    {
+        private readonly ITarefasRepository _tarefasRepository;
+
+        public EditarTarefaHandler(ITarefasRepository tarefasRepository)
+        {
+            _tarefasRepository = tarefasRepository;
+        }
+
+        public async Task<Tarefa> Handle(EditarTarefaCommand request, CancellationToken cancellationToken)
+        {
+            var tarefa = await _tarefasRepository.GetByIdAsync(request.Id);
+
+            tarefa!.Titulo = request.Titulo;
+            tarefa.Status = request.Status;
+            tarefa.Descricao = request.Descricao;
+            tarefa.DataVencimento = request.DataVencimento;
+
+            await _tarefasRepository.updateAsync(tarefa);
+
+            return tarefa;
+        }
+    }
+}
