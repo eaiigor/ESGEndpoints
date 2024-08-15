203accc5ce516dca3c2a43bc5c9be017b3d534ff
Igor Rocha Lima <contatoigorrochalima@hotmail.com>
2024-08-15 01:12:14 -0300

    feat(ApiDbContext): Criação DbContext + Migrations

---
+++ Infrastructure/Data/ApiDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ESGENDPOINTS.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ESGENDPOINTS.Infrastructure.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Tarefa> Tarefa { get; set; }
+    }
+}
