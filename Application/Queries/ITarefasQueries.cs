ee33a83ca4f8a06b3fd7b94c819464f9aba5daa5
Igor Rocha Lima <contatoigorrochalima@hotmail.com>
2024-08-15 01:14:14 -0300

    feat(TarefasQueries): Criando queries base

---
+++ Application/Queries/ITarefasQueries.cs
@@ -0,0 +1,11 @@
+﻿using ESGENDPOINTS.Domain.Entities;
+
+namespace ESGENDPOINTS.Application.Queries
+{
+    public interface ITarefasQueries
+    {
+        Task<Tarefa?> GetByIdAsync(int id);
+        Task<IEnumerable<Tarefa>> GetAllAsync();
+        Task<bool> ExistsAsync(int id);
+    }
+}
---
+++ Application/Queries/TarefasQueries.cs
@@ -0,0 +1,21 @@
+﻿using ESGENDPOINTS.Domain.Entities;
+using ESGENDPOINTS.Infrastructure.Repositories;
+
+namespace ESGENDPOINTS.Application.Queries
+{
+    public class TarefasQueries : ITarefasQueries
+    {
+        private readonly ITarefasRepository _tarefasRepository;
+
+        public TarefasQueries(ITarefasRepository tarefasRepository)
+        {
+            _tarefasRepository = tarefasRepository;
+        }
+
+        public Task<IEnumerable<Tarefa>> GetAllAsync() => _tarefasRepository.GetAllAsync();
+
+        public Task<Tarefa?> GetByIdAsync(int id) => _tarefasRepository.GetByIdAsync(id);
+
+        public Task<bool> ExistsAsync(int id) => _tarefasRepository.ExistsAsync(id);
+    }
+}
