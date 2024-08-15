2992cd1294be222defdfadb76444d0c503f68eec
Igor Rocha Lima <contatoigorrochalima@hotmail.com>
2024-08-15 01:12:57 -0300

    feat(Tarefa): Criando entidade Tarefa

---
+++ Domain/Entities/Tarefa.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ESGENDPOINTS.Domain.Entities
+{
+    [Table("Tarefa")]
+    public class Tarefa
+    {
+        [Column("Id")]
+        [Display(Name = "ID")]
+        public int Id { get; set; }
+
+        [Column("Titulo")]
+        [Display(Name = "Título")]
+        public string Titulo { get; set; }
+
+        [Column("Descricao")]
+        [Display(Name = "Descrição")]
+        public string? Descricao { get; set; }
+
+        [Column("DataVencimento")]
+        [Display(Name = "Data de Vencimento")]
+        public DateTime DataVencimento { get; set; }
+
+        [Column("Status")]
+        [Display(Name = "Status")]
+        public int Status { get; set; }
+    }
+}
