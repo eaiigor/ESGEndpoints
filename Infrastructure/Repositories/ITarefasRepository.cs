a47a1b799010bf9fb2d652559260d94c0d6ea3c5
Igor Rocha Lima <contatoigorrochalima@hotmail.com>
2024-08-15 01:13:15 -0300

    feat(TarefasRepository): criando repositório de tarefas

---
+++ Infrastructure/Repositories/ITarefasRepository.cs
@@ -0,0 +1,14 @@
+﻿using ESGENDPOINTS.Domain.Entities;
+
+namespace ESGENDPOINTS.Infrastructure.Repositories
+{
+    public interface ITarefasRepository
+    {
+        Task<IEnumerable<Tarefa>> GetAllAsync();
+        Task<Tarefa?> GetByIdAsync(int id);
+        Task<Tarefa> createAsync(Tarefa tarefa);
+        Task<Tarefa> updateAsync(Tarefa tarefa);
+        Task DeleteAsync(int id);
+        Task<bool> ExistsAsync(int id);
+    }
+}
---
+++ Infrastructure/Repositories/TarefasRepository.cs
@@ -0,0 +1,59 @@
+﻿using ESGENDPOINTS.Domain.Entities;
+using ESGENDPOINTS.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ESGENDPOINTS.Infrastructure.Repositories
+{
+    public class TarefasRepository : ITarefasRepository
+    {
+        private readonly ApiDbContext _context;
+
+        public TarefasRepository(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Tarefa> createAsync(Tarefa tarefa)
+        {
+            _context.Tarefa.Add(tarefa);
+            await _context.SaveChangesAsync();
+
+            return tarefa;
+            
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var tarefa = await _context.Tarefa.FindAsync(id);
+            if (tarefa == null)
+            {
+                return;
+            }
+
+            _context.Tarefa.Remove(tarefa);
+            await _context.SaveChangesAsync();
+            return;
+        }
+
+        public async Task<IEnumerable<Tarefa>> GetAllAsync()
+        {
+            return await _context.Tarefa.ToListAsync();
+        }
+
+        public async Task<Tarefa?> GetByIdAsync(int id)
+        {
+            var tarefa = await _context.Tarefa.FindAsync(id);
+            return tarefa;
+        }
+
+        public async Task<Tarefa> updateAsync(Tarefa tarefa)
+        {
+            _context.Entry(tarefa).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return tarefa;
+        }
+
+        public Task<bool> ExistsAsync(int id) => _context.Tarefa.AnyAsync(e => e.Id == id);
+    }
+}
